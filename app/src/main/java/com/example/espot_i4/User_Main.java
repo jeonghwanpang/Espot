279e91045901730febce2b750306863c050f43a4
zooromi <163655707+zooromi@users.noreply.github.com>
2024-03-26 11:19:12 +0900

    user main 쪼끔 만졌어용

--- app/src/main/java/com/example/espot_i4/User_Main.java
+++ app/src/main/java/com/example/espot_i4/User_Main.java
@@ -15,12 +15,32 @@ public class User_Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_main);
 
-        Button naverButton = (Button) findViewById(R.id.homebutton);
-        naverButton.setOnClickListener(new View.OnClickListener() {
+        // 충전소 찾기
+        Button findButton = (Button) findViewById(R.id.findbutton);
+        findButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                //화면 전환
+                Intent intent = new Intent(getApplicationContext(), find_cs.class);
+                startActivity(intent);
+            }
+        });
+
+        // 사용 가능한 충전기 확인
+        Button checkcgButton = (Button) findViewById(R.id.checkcgbutton);
+        checkcgButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                //화면 전환
+                Intent intent = new Intent(getApplicationContext(), check_charger.class);
+                startActivity(intent);
+            }
+        });
+
+        // 홈 버튼
+        Button homeButton = (Button) findViewById(R.id.homebutton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view){
                 //화면 전환
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                //Intent intent = new Intent(getApplicationContext(), User_Main.class);
                 startActivity(intent);
             }
         });
