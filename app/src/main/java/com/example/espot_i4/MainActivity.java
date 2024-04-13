c1e81c98ccc4c1c25f31be3b5a8c094a155c810b
SUBIN <wesongbbare@gmail.com>
2024-04-14 04:14:52 +0900

    DB연동 + 회원가입 UI 추가 (완벽x)

--- app/src/main/java/com/example/espot_i4/MainActivity.java
+++ app/src/main/java/com/example/espot_i4/MainActivity.java
@@ -4,40 +4,94 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth; // 파이어 베이스 연동
+    private DatabaseReference mDatabaseRef; // 실시간 데이터 베이스
+    private EditText mEtID, mEtPwd, mEtName, mEtPhone, mEtCar1, mEtCar2, mEtAdminNum;
+    private Button mBtnRegister, mBtnLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //<사용자 로그인> 카카오 버튼 누를 시 차량 번호 입력 화면으로 전환
-        Button imageButton = (Button) findViewById(R.id.kakaobutton);
-        imageButton.setOnClickListener(new View.OnClickListener() {
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+
+        mEtID = findViewById(R.id.editTextText3);
+        mEtPwd = findViewById(R.id.editTextText4);
+        mBtnRegister = findViewById(R.id.JoinButton);
+        mBtnLogin = findViewById(R.id.LoginButton);
+
+        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strID = mEtID.getText().toString();
+                String strPwd = mEtPwd.getText().toString();
+                String strName = mEtName.getText().toString();
+                String strPhone = mEtPhone.getText().toString();
+                String strCar1 = mEtCar1.getText().toString();
+                String strCar2 = mEtCar2.getText().toString();
+                //String strAdminNum = mEtAdminNum.getText().toString();
+
+                mFirebaseAuth.signInWithEmailAndPassword(strID, strPwd).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // 로그인 성공
+                            Intent intent = new Intent(MainActivity.this, User_Main.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(MainActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
+
+
+        //<사용자 로그인> 회원 가입 버튼 누를 시 사용자 모드 회원 가입 화면으로 전환
+        Button joinButton = (Button) findViewById(R.id.JoinButton);
+        joinButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 //화면 전환
-                Intent CarNumintent = new Intent(getApplicationContext(), CarNum.class);
-                //Intent intent = new Intent(getApplicationContext(), User_Main.class);
-                startActivity(CarNumintent);
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
             }
         });
 
-        //<사용자 로그인> 네이버 버튼 누를 시 차량 번호 입력 화면으로 전환
-        Button naverButton = (Button) findViewById(R.id.naverbutton);
-        naverButton.setOnClickListener(new View.OnClickListener() {
+        /*
+        // 임시
+        //<사용자 로그인> 로그인 버튼 누를 시 관리자 모드 회원 가입 화면으로 전환
+        Button loginButton = (Button) findViewById(R.id.LoginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view){
                 //화면 전환
-                Intent CarNumintent = new Intent(getApplicationContext(), CarNum.class);
-                //Intent intent = new Intent(getApplicationContext(), User_Main.class);
-                startActivity(CarNumintent);
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity_Admin.class);
+                startActivity(intent);
             }
         });
-
+*/
 
     }
 }
---
+++ app/src/main/java/com/example/espot_i4/RegisterActivity_Admin.java
@@ -0,0 +1,82 @@
+package com.example.espot_i4;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterActivity_Admin extends AppCompatActivity {
+    private FirebaseAuth mFirebaseAuth; // 파이어 베이스 연동
+    private DatabaseReference mDatabaseRef; // 실시간 데이터 베이스
+    private EditText mEtID, mEtPwd, mEtName, mEtPhone, mEtAdminNum;
+    private Button mBtnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register_admin);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+
+        mEtID = findViewById(R.id.et_email);
+        mEtPwd = findViewById(R.id.et_pwd);
+        mEtName = findViewById(R.id.et_name);
+        mEtPhone = findViewById(R.id.et_phone);
+        mEtAdminNum = findViewById(R.id.et_adminNum);
+        mBtnRegister = findViewById(R.id.btn_registerAdmin);
+
+        mBtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 회원가입 처리 시작
+                String strID = mEtID.getText().toString();
+                String strPwd = mEtPwd.getText().toString();
+                String strName = mEtName.getText().toString();
+                String strPhone = mEtPhone.getText().toString();
+                String strAdminNum = mEtAdminNum.getText().toString();
+
+                // Firebase Auth 진행
+                mFirebaseAuth.createUserWithEmailAndPassword(strID, strPwd).addOnCompleteListener(RegisterActivity_Admin.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+                            UserAccount account = new UserAccount();
+                            account.setIdToken(firebaseUser.getUid());
+                            account.setEmailId(firebaseUser.getEmail());
+                            account.setPassword(strPwd);
+
+                            mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
+                            Toast.makeText(RegisterActivity_Admin.this, "회원가입에 성공했습니다", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(RegisterActivity_Admin.this, "회원가입에 실패했습니다", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
